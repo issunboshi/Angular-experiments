0c2fc96beb31b643f6e52d61536b192e2f7fda07
Cliff Williams <cliffordw1987@gmail.com>
2014-12-24 19:43:40 +0000

    ng-repeat used and secondary controller in place to control tabbed interface

--- js/app.js
+++ js/app.js
@@ -1,23 +1,74 @@
-var sampleStore = angular.module('sampleStore', []);
+(function() {
+  var app = angular.module('sampleStore', []);
 
-// Used to share assets with different controllers without having to share a parent method which holds the same data
-sampleStore.factory('Message', function () {
-	return [
-		{
-			message: "I'm data from a service",
-			someOtherData: "Hello"
-		},
-		{
-			message: "I'm some more data from a service",
-			someOtherData: "There"
-		}
-	];
-});
+  /**
+   * Generic controller for the store
+   * @param  {Array} The store product data
+   */
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
 
-sampleStore.controller('FirstCtrl', function ($scope, Message) {
-	$scope.data = Message;
-});
+/**
+ * Set and get the current active tab value
+ * @param  {Number} this.tab The initial value for the tab
+ */
+  app.controller('TabController', function(){
+    this.tab = 1;
 
-sampleStore.controller('SecondCtrl', function ($scope, Message) {
-	$scope.data = {message: "Hello"};
-});
+    /**
+     * Take a value from a directive and set this.tab to it
+     * @param {Number} newValue The new value for this.tab
+     */
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    /**
+     * Take a value from a directive and compare the current tab value to it
+     * @param  {Number}  tabName The value passed by the directive
+     * @return {Boolean} Does the directive's value match the current value of this.tab
+     */
+    this.isSet = function(tabName){
+    	console.log(this.tab);
+      return this.tab === tabName;
+    };
+  });
+
+ var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [ ]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "images/gem-01.gif",
+      "images/gem-03.gif",
+      "images/gem-04.gif"
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-09.gif"
+    ]
+  }];
+})();
